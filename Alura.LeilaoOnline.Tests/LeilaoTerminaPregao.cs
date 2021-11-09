8c9035221746da6885c52232fc2976622cfe6709
Ryan Lemos <ryanlucaslemos@hotmail.com>
2021-11-09 07:48:10 -0300

    Inclui melhoria no design baseado nos testes

--- Alura.LeilaoOnline.Tests/LeilaoTerminaPregao.cs
+++ Alura.LeilaoOnline.Tests/LeilaoTerminaPregao.cs
@@ -11,6 +11,42 @@ namespace Alura.LeilaoOnline.Tests
     public class LeilaoTerminaPregao
     {
         [Theory]
+        [InlineData(1200, 1250, new double[] { 800, 1150, 1400, 1250})]
+        public void RetornaValorSuperiorMaisProximoDadoLeilaoNessaModalidade(double valorDestino, double valorEsperado, double[] ofertas)
+        {
+            //Arrange - Cenário
+            var leilao = new Leilao("Van Gogh", valorDestino);
+            var fulano = new Interessada("Fulano", leilao);
+
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.IniciaPregao();
+
+            for (int i = 0; i < ofertas.Length; i++)
+            {
+                var valor = ofertas[i];
+                if ((i % 2) == 0)
+                {
+                    leilao.RecebeLance(fulano, valor);
+                }
+                else
+                {
+                    leilao.RecebeLance(maria, valor);
+                }
+            }
+
+            //Act - metodo sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+
+        [Theory]
         [InlineData(1200, new double[] { 800, 900, 1000, 1200 })]
         [InlineData(1000, new double[] { 800, 900, 1000, 990 })]
         [InlineData(800, new double[] { 800 })]
